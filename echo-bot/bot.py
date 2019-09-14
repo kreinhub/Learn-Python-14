d169a45a0e3cf425a8e5279ad4d444c654cea22c
krein <sirionhvg@gmail.com>
2019-09-14 14:01:16 +0300

    Send the homework about ephem and bot

--- echo-bot/bot.py
+++ echo-bot/bot.py
@@ -1,6 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
+import datetime
 
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', 
@@ -23,6 +25,27 @@ def talk_to_me(bot, update):
     logging.info(f"User: {username}, Chat id: {chat_id}, Message: {text}")
     update.message.reply_text(user_text)
 
+def planet(bot, update):
+    list_text = update.message.text.split()
+    planet_name = list_text[1].lower().capitalize()
+
+    short_date = datetime.datetime.now().strftime("%x")
+    try:
+        planet_object = getattr(ephem, planet_name)(short_date)
+        const = ephem.constellation(planet_object)
+        bot_answer = f"{planet_name} is in {const[1]} constellation today"
+    
+        log_text = f"/planet is called with arg {planet_name}"
+        logging.info(log_text)
+        update.message.reply_text(bot_answer)
+    except AttributeError:
+        planets = "Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune"
+        bot_answer = f"There is no such planet in our galaxy. Try to enter following ones: {planets}"
+
+        logging.info("[Attribute Error] /planet gets wrong planet name")
+        update.message.reply_text(bot_answer)
+
+
 def main():
     mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
 
@@ -30,6 +53,7 @@ def main():
 
     dp = mybot.dispatcher 
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
@@ -39,5 +63,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-    
-Consolas, 'Courier New', monospace
