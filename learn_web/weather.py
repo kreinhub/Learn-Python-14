e003bf8ed7e239eda2954c520290181a4e146ddb
krein <sirionhvg@gmail.com>
2019-09-22 13:42:12 +0300

    add bootstrap and news with beatiful soup

--- learn_web/weather.py
+++ learn_web/weather.py
@@ -10,15 +10,21 @@ def weather_by_city(city_name):
         "num_of_days": 1,
         "lang": "ru"
     }
-    result = requests.get(weather_url, params=params)
-    weather = result.json()
-    if "data" in weather:
-        if "current_condition" in weather["data"]:
-            try:
-                return weather["data"]["current_condition"][0]
-            except(IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if "data" in weather:
+            if "current_condition" in weather["data"]:
+                try:
+                    return weather["data"]["current_condition"][0]
+                except(IndexError, TypeError):
+                    return False
+    except(requests.RequestException, ValueError):
+        print("Network Error")
+        return False
     return False
 
 if __name__ == "__main__":
     print(weather_by_city("Moscow, Russia"))
+    
