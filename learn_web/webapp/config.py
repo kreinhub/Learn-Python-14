f8f5ad28998905e6d6fdc12ea0c361c658841cdf
krein <sirionhvg@gmail.com>
2019-09-29 18:07:27 +0300

    Add refactoring and config.py

---
+++ learn_web/webapp/config.py
@@ -0,0 +1,3 @@
+WEATHER_DEFAULT_CITY = "Moscow, Russia"
+WEATHER_API_KEY = "58a7623265c24006806102343191709"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
--- learn_web/webapp/weather.py
+++ learn_web/webapp/weather.py
@@ -1,10 +1,11 @@
+from flask import current_app
 import requests
 
 
 def weather_by_city(city_name):
-    weather_url = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+    weather_url = current_app.config["WEATHER_URL"]
     params = {
-        "key": "58a7623265c24006806102343191709",
+        "key": current_app.config["WEATHER_API_KEY"],
         "q": city_name,
         "format": "json",
         "num_of_days": 1,
